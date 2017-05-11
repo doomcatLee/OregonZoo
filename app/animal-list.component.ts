220c66a7d4a379a352131870288da0a6113c5e15
Alex Lee <doomcat9167@gmail.com>
2017-05-11 10:38:26 -0700

    low caretakers pipe works now

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -56,6 +56,28 @@ import { NewAnimalComponent } from './new-animal.component';
   </div>
 </div>
 </div>
+
+<div *ngIf="filterByMaster==='lowCareTakers'">
+<div class="col-lg-4 col-sm-6 text-center" *ngFor="let currentAnimal of childAnimalList | masterPipe:filterByMaster" style="margin-top: 100px;">
+<div class="well">
+  <img class="img-circle img-responsive img-center" src= "{{currentAnimal.imgUrl}}" alt="">
+  <div class="panel-heading">{{currentAnimal.name}}</div>
+  <div class="panel-body">
+  <ul>
+    <li>Species: {{currentAnimal.species}}</li>
+    <li>Age: {{currentAnimal.age}}</li>
+    <li>Diet: {{currentAnimal.diet}}</li>
+    <li>Location: {{currentAnimal.location}}</li>
+    <li>Number of caretakers: {{currentAnimal.caretakers}}</li>
+    <li>Sex: {{currentAnimal.sex}}</li>
+  </ul>
+  <button class="btn btn-default" data-toggle="modal" data-target="#editModal" (click)="editButtonHasBeenClicked(currentAnimal)">Edit</button>
+
+  </div>
+</div>
+</div>
+</div>
+
   `
 })
 
--- app/master.pipe.ts
+++ app/master.pipe.ts
@@ -9,22 +9,29 @@ import { Animal } from './animal.model';
 
 export class MasterPipe implements PipeTransform {
 
-    transform(input: Animal[], desiredMaster) {
-      var output: Animal[] = [];
-      if(desiredMaster === "all") {
-        for (var i = 0; i < input.length; i++) {
-            output.push(input[i]);
-        }
-        return output;
-      } else if (desiredMaster === "youngAnimal") {
-        for (var i = 0; i < input.length; i++) {
-          if (input[i].age < 7){
-            output.push(input[i]);
-          }
-        }
-        return output;
-      } else {
-        return input;
+  transform(input: Animal[], desiredMaster) {
+    var output: Animal[] = [];
+    if (desiredMaster === "all") {
+      for (var i = 0; i < input.length; i++) {
+        output.push(input[i]);
       }
+      return output;
+    } else if (desiredMaster === "youngAnimal") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].age < 7) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else if (desiredMaster === "lowCareTakers") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].caretakers < 3) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else {
+      return input;
     }
+  }
 }
